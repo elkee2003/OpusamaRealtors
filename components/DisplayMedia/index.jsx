36adef63bee1929796b545bd0504d75018bc171d
Elkanah <elkee2003@yahoo.com>
2024-06-27 00:16:37 +0100

    fixed the video duration and position

--- components/DisplayMedia/index.jsx
+++ components/DisplayMedia/index.jsx
@@ -15,9 +15,17 @@ const DisplayMedia = () => {
   const {media, removeMedia} = useUploadContext()
   const videoRefs = useRef([]);  // Array to hold references to Video components
   const [isPlaying, setIsPlaying] = useState([]);
-  const [showControls, setShowControls] = useState(true);
-  const [videoDuration, setVideoDuration] = useState(0);
-  const [videoPosition, setVideoPosition] = useState(0);
+  const [showControlsState, setShowControlsState] = useState([]);
+  const [videoDurations, setVideoDurations] = useState([]);
+  const [videoPositions, setVideoPositions] = useState([]);
+
+  // Initialize showControlsState based on number of media items
+  useEffect(() => {
+    setShowControlsState(Array(media.length).fill(false));
+    setIsPlaying(Array(media.length).fill(false));
+    setVideoDurations(Array(media.length).fill(0));
+    setVideoPositions(Array(media.length).fill(0));
+  }, [media]);
 
   // This useFocusEffect is to pause the video, when navigate to another screen
   useFocusEffect(
@@ -26,22 +34,36 @@ const DisplayMedia = () => {
         // Stop all videos when the screen loses focus
         videoRefs.current.forEach(async (videoRef) => {
           if (videoRef) {
-            await videoRef.pauseAsync();
+            try{
+              await videoRef.pauseAsync();
+            }catch(error){
+              console.error("Error pausing video", error)
+            }
           }
         });
         setIsPlaying([]);
+        setShowControlsState(Array(media.length).fill(false));
       };
-    }, [])
+    }, [media])
   );
 
-  // Toggle controls visibility on interaction
-  const toggleControls = () => {
-    setShowControls(true);
-    // Reset the timer to hide controls after 3 seconds of inactivity
-    clearTimeout(controlTimer);
-    controlTimer = setTimeout(() => {
-        setShowControls(false);
-    }, 3000); // Adjust the duration (in milliseconds) as needed
+  // Toggle controls visibility on interaction for a specific video
+  const toggleControls = (index) => {
+    setShowControlsState((prev) => {
+      const newShowControlsState = [...prev];
+      newShowControlsState[index] = true;
+      return newShowControlsState;
+    });
+
+  // Reset the timer to hide controls after 3 seconds of inactivity
+  clearTimeout(controlTimers[index]);
+  controlTimers[index] = setTimeout(() => {
+    setShowControlsState((prev) => {
+      const newShowControlsState = [...prev];
+      newShowControlsState[index] = false;
+      return newShowControlsState;
+    });
+  }, 3000); // Adjust the duration (in milliseconds) as needed
   };
 
   // Function to handle play/pause, fastward and rewind below
@@ -49,64 +71,92 @@ const DisplayMedia = () => {
   // Function to handle play/pause a video
   const handlePlayPause = async (index)=>{
     const videoRef = videoRefs.current[index];
-    if (isPlaying[index]){
-      await videoRef.pauseAsync();
-    }else {
-      await videoRef.playAsync();
-    }
-    setIsPlaying((prev)=>{
-      const newPlayingState = [...prev];
-      newPlayingState[index] = !newPlayingState[index];
-      return newPlayingState;
-    });
-    toggleControls(); // Show controls when playing/pausing
+    try {if (isPlaying[index]){
+        await videoRef.pauseAsync();
+      }else {
+        await videoRef.playAsync();
+      }
+      setIsPlaying((prev)=>{
+        const newPlayingState = [...prev];
+        newPlayingState[index] = !newPlayingState[index];
+        return newPlayingState;
+      });
+      toggleControls(index); // Show controls when playing/pausing
+      }catch(error){
+        console.error('Error playing/pausing video:', error);
+      }
   };
 
   // Function to handle fastforward a video
   const handleFastForward = async (index) => {
     const videoRef = videoRefs.current[index];
-    const status = await videoRef.getStatusAsync();
-    if (status.isLoaded) {
-      const newPosition = status.positionMillis + 5000; // fast forward by 5 seconds
-      await videoRef.setPositionAsync(newPosition);
+    try{
+       const status = await videoRef.getStatusAsync();
+      if (status.isLoaded) {
+        const newPosition = status.positionMillis + 5000; // fast forward by 5 seconds
+        await videoRef.setPositionAsync(newPosition);
+      }
+      toggleControls(index); // Show controls when fast forwarding
+    }catch(error){
+      console.error('Error fastforwarding video:', error)
     }
-    toggleControls(); // Show controls when fast forwarding
+   
   };
 
   // Function to handle rewinding a video
   const handleRewind = async (index) => {
     const videoRef = videoRefs.current[index];
-    const status = await videoRef.getStatusAsync();
-    if (status.isLoaded) {
-      const newPosition = Math.max(status.positionMillis - 5000, 0); // rewind by 5 seconds
-      await videoRef.setPositionAsync(newPosition);
+    try{
+      const status = await videoRef.getStatusAsync();
+      if (status.isLoaded) {
+        const newPosition = Math.max(status.positionMillis - 5000, 0); // rewind by 5 seconds
+        await videoRef.setPositionAsync(newPosition);
+      }
+      toggleControls(index); // Show controls when rewinding
+    }catch(error){
+      console.error('Error rewinding video:', error)
     }
-    toggleControls(); // Show controls when rewinding
   };
 
   // Function to update video position and duration
   const updateVideoStatus = async (index) => {
     const videoRef = videoRefs.current[index];
-    const status = await videoRef.getStatusAsync();
-    if (status.isLoaded) {
-        setVideoPosition(status.positionMillis);
-        setVideoDuration(status.durationMillis);
+    try{
+        const status = await videoRef.getStatusAsync();
+        if (status.isLoaded) {
+          setVideoPositions((prev) => {
+            const newVideoPositions = [...prev];
+            newVideoPositions[index] = status.positionMillis;
+            return newVideoPositions;
+          });
+          setVideoDurations((prev) => {
+            const newVideoDurations = [...prev];
+            newVideoDurations[index] = status.durationMillis;
+            return newVideoDurations;
+          });
+      }
+    }catch(error){
+      console.error('Error updating video status:', error)
     }
   };
 
   // Timer to hide controls after a period of inactivity
-  let controlTimer;
+  const controlTimers = useRef([])
 
   useEffect(() => {
+    // Initialize controlTimers array
+    controlTimers.current = Array(media.length).fill(null);
+
     // Update video status every second
-    progressTimer = setInterval(() => {
-        updateVideoStatus(0); // Assuming single video for simplicity, adjust if needed
-    }, 1000);
+    const progressTimers = media.map((_, index) => setInterval(() => {
+      updateVideoStatus(index);
+    }, 1000));
 
     return () => {
-        clearInterval(progressTimer); // Clear the timer on unmount
+      progressTimers.forEach((timer) => clearInterval(timer)); // Clear the timers on unmount
+      controlTimers.current.forEach((timer) => clearTimeout(timer));
     };
-}, []);
+}, [media]);
   
   // Function Render Items for Flatlist
   const renderItem = ({item, index})=>{
@@ -138,14 +188,14 @@ const DisplayMedia = () => {
                 useNativeControls={false}
                 resizeMode='contain'
                 isLooping
-                onTouchStart={toggleControls}
+                onTouchStart={()=> toggleControls(index)}
               />
 
               {/* If I want to use an Icon to control the Pause and Play */}
               {/* <MaterialIcons style={styles.controlbtn} name={isPlaying[index] ? "pause" : "play-arrow"} /> */}
 
               {/* Controls container */}
-            {showControls && <View style={styles.videoOverlayContainer}>
+            {showControlsState[index] && <View style={styles.videoOverlayContainer}>
               <TouchableOpacity style={styles.videoOverlayLeft} onPress={() => handleRewind(index)} >
                 <Entypo style={styles.controlbtn} name="controller-fast-backward"/>
               </TouchableOpacity>
@@ -160,9 +210,9 @@ const DisplayMedia = () => {
             </View>}
 
             {/* Progress bar */}
-            {showControls && (
+            {showControlsState[index] && (
                 <View style={styles.progressBar}>
-                    <View style={[styles.progress, { width: (videoPosition / videoDuration) * 100 + '%' }]} />
+                    <View style={[styles.progress, { width: (videoPositions[index] / videoDurations[index]) * 100 + '%' }]} />
                 </View>
             )}
           </View>
